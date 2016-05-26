08646180ae42a8724815e7b7af40c0b530fd3357
Thor Keller <kellerw@users.noreply.github.com>
2016-05-26 16:41:07 -0400

    Code is now proper

--- Player.java
+++ Player.java
@@ -7,14 +7,15 @@ import java.awt.*;
 
 public class Player
 {
-   private static Territory terr1, terr2;
+   private static Territory terr1, terr2; // The attacking and defending territories
    private static int unitsToAdd = 0;
-   private static int currPhase = 1;
+   private static int currPhase = 0;
    private static ArrayList<Continent> continents = getContinents();
    private static int currPlayer = 0;
    private static ArrayList<Player> players = new ArrayList<Player>();
    private static int[] cardBonuses = {4,6,8,10,12,15};
    private static int horsePos = 0;
+   private static boolean captured = false;
    private LinkedList<Territory> territories;
    private Deck hand;
    private Color color;
@@ -32,10 +33,10 @@ public class Player
    //-------------------------------------------
    private static ArrayList<Continent> getContinents()
    {
+      ArrayList<Continent> continents = new ArrayList<Continent>();
       try
       {
          Scanner scan = new Scanner(new java.io.File("Continents.txt"));
-         ArrayList<Continent> continents = new ArrayList<Continent>();
          for(int i = 0; i < 6; i++)
          {
             String name = scan.nextLine();
@@ -61,7 +62,7 @@ public class Player
    //Calculates the number of units the Player should receive
    //@return Number of units the Player should receive
    //-------------------------------------------
-   public void getUnits(boolean usedCards)
+   public int getUnits(boolean usedCards)
    {
       int units = 0;
       if(territories.size() < 9)
@@ -81,6 +82,7 @@ public class Player
             units += (15 + (horsePos - 6)*5);
          horsePos++;
       }
+      return(units);
    }
    //-------------------------------------------
    //Adds a player to the list of players in the game
@@ -132,50 +134,132 @@ public class Player
    //-------------------------------------------
    //viewHand Draws the cards in the Player object's hands to the play screen
    //-------------------------------------------
-    public void viewHand()
-    {
+   public void viewHand()
+   {
       handVisible = !handVisible;
       hand.setVisible(handVisible);
-    }
+   }
     //-------------------------------------------
-    //
+    // Sets the turn to the next phase
     //-------------------------------------------
-    public static void incrementCurrPhase()
-    {
+   public static void incrementCurrPhase()
+   {
       currPhase++;
       if(currPhase > 3)
       {
          currPlayer = (currPlayer+1)%players.size();
          currPhase = 1;
+         Button.changeLabel("invisible");
       }
-    }
+      if(currPhase == 3)
+         Button.changeLabel("End");
+      captured = false;
+      if(terr1!=null)
+         terr1.setSelected(false);
+      if(terr2!=null)
+         terr2.setSelected(false);
+      terr1 = null;
+      terr2 = null;
+      if(currPhase == 1)
+      //change to check if cards are used?
+         unitsToAdd = players.get(currPlayer).getUnits(false);
+   }
+    //-------------------------------------------
+    // Resets the selected cities.
+    //-------------------------------------------
+   public static void resetSelected(int lossesattacker, int lossesdefender, int selected)
+   {
+      if(terr1!=null)
+         terr1.setSelected(false);
+      if(terr2!=null)
+         terr2.setSelected(false);
+      terr1.removeUnits(lossesattacker);
+      terr2.removeUnits(lossesdefender);
+      if(terr2.getUnits()==0)
+      {
+         terr1.removeUnits(selected);
+         terr2.setOwner(terr1.getOwner());
+         //check if the player is eliminated
+         terr2.addUnits(selected);
+         Map.miniUpdate(terr2);
+         if(!captured)
+         {
+            //insert part to draw a card
+            captured = true;
+         }
+      }
+      terr1 = null;
+      terr2 = null;
+   }
    //-------------------------------------------
    //Processes the turn.
    //-------------------------------------------
-    public static void processTurn()
-    {
+   public static void processTurn()
+   {
       if(currPhase == 1)
       {
-         if(Map.getSelectedTerritory() != null && Map.getSelectedTerritory().getPlayer() == players.get(currPlayer))
+         if(Map.getSelectedTerritory() != null && Map.getSelectedTerritory().getOwner() == players.get(currPlayer))
          {
-            Map.getSelectedTerritory().addUnit(1);
+            Map.getSelectedTerritory().addUnits(1);
             unitsToAdd--;
          }
-         if(unitsToAdd == 0) 
+         if(unitsToAdd == 0)
+         {
             incrementCurrPhase();
+            Button.changeLabel("Done");
+         }
       }
       else if(currPhase == 2)
       {
-         if(Map.getSelectedTerritory() != null && Map.getSelectedTerritory().getPlayer() == players.get(currPlayer))
-            
+         if(terr1 == null && Map.getSelectedTerritory() != null && Map.getSelectedTerritory().getOwner() == players.get(currPlayer)&&Map.getSelectedTerritory().getUnits()>1)
+         {
+            terr1 = Map.getSelectedTerritory();
+            terr1.setSelected(true);
+         }
+         else if (terr1 != null && Map.getSelectedTerritory() != null && terr2==null && terr1.borders(Map.getSelectedTerritory())&& Map.getSelectedTerritory().getOwner() != players.get(currPlayer))
+         {
+            terr2 = Map.getSelectedTerritory();
+            terr2.setSelected(true);
+         }
+         else if (terr1 != null && Map.getSelectedTerritory() != null && Map.getSelectedTerritory()==terr1 && terr2 == null)
+         {
+            terr1.setSelected(false);
+            terr1 = null;
+         }
+         if(terr1 != null && terr2 != null)
+         {
+            Die.showDice(terr1.getUnits(),terr2.getUnits());
+         }
       }
       else if(currPhase == 3)
       {
-         
+         if(terr1 == null && Map.getSelectedTerritory() != null && Map.getSelectedTerritory().getOwner() == players.get(currPlayer) && Map.getSelectedTerritory().getUnits()>1)
+         {
+            terr1 = Map.getSelectedTerritory();
+            terr1.setSelected(true);
+         }
+         else if (terr1 != null && Map.getSelectedTerritory() != null && terr2==null && terr1.borders(Map.getSelectedTerritory())&& Map.getSelectedTerritory().getOwner() == players.get(currPlayer))
+         {
+            terr2 = Map.getSelectedTerritory();
+            terr2.setSelected(true);
+         }
+         else if (terr1 != null && terr1 == null && Map.getSelectedTerritory() != null && Map.getSelectedTerritory()==terr1)
+         {
+            terr1.setSelected(false);
+            terr1 = null;
+         }
+         if(terr1 != null && terr2 != null && terr1.getUnits()>0 && Map.getSelectedTerritory()==terr2)
+         {
+            terr1.fortify(terr2,1);
+         }
       }
-    }
-    public static Player getCurrentPlayer()
-    {
-      return(players.get());
-    }
+   }
+   //-------------------------------------------
+   //Returns the player whose turn it is
+   //@return the current player
+   //-------------------------------------------
+   public static Player getCurrentPlayer()
+   {
+      return(players.get(currPlayer));
+   }
 }
